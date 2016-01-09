9d0980136ed330c60321712fd2b87d0947815ce8
Daniel Berkompas <himself@danielberkompas.com>
2016-01-09 14:34:28 -0800

    Install `standard` for Javascript linting

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,6 +1,6 @@
 // Brunch automatically concatenates all files in your
 // watched paths. Those paths can be configured at
-// config.paths.watched in "brunch-config.js".
+// config.paths.watched in 'brunch-config.js'.
 //
 // However, those files will only be executed if
 // explicitly imported. The only exception are files
@@ -10,12 +10,12 @@
 // Import dependencies
 //
 // If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "deps/phoenix_html/web/static/js/phoenix_html"
+// to also remove its path from 'config.paths.watched'.
+import 'deps/phoenix_html/web/static/js/phoenix_html'
 
 // Import local files
 //
 // Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
+// paths './socket' or full ones 'web/static/js/socket'.
 
-// import socket from "./socket"
+// import socket from './socket'
