23db00760a4551d792354c1d93b00158fc3afdcf
Daniel Berkompas <himself@danielberkompas.com>
2016-01-15 07:33:11 -0800

    Add Semaphore support to test database config

--- config/test.exs
+++ config/test.exs
@@ -10,8 +10,13 @@ config :phoenix_base, PhoenixBase.Endpoint,
 config :logger, level: :warn
 
 # Configure your database
+{whoami, _} = System.cmd("whoami", [])
+whoami = String.replace(whoami, "\n", "")
+
 config :phoenix_base, PhoenixBase.Repo,
   adapter: Ecto.Adapters.Postgres,
   database: "phoenix_base_test",
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || whoami,
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || nil,
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
