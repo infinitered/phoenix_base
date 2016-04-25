a28ea9c1000e9a662bbf9e0a7eabef5ac1a8f91a
Daniel Berkompas <himself@danielberkompas.com>
2016-04-25 09:01:05 -0700

    [#8] Replace EEX with Slime
    Note that if you want to generate controllers, you'll need to use the
    slime generators, such as:
    
        mix phoenix.gen.html.slime

--- config/dev.exs
+++ config/dev.exs
@@ -21,7 +21,7 @@ config :phoenix_base, PhoenixBase.Endpoint,
       ~r{priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$},
       ~r{priv/gettext/.*(po)$},
       ~r{web/views/.*(ex)$},
-      ~r{web/templates/.*(eex)$}
+      ~r{web/templates/.*(eex|slim)$}
     ]
   ]
 
